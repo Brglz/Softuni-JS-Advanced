9873f5343e22742e9e5d2fe68257a57472eca256
StelianDimitrov <Brglz@users.noreply.github.com>
2020-05-20 15:31:11 +0300

    Update excercise.js
    Made change to Road Radar Excercise

--- 01. SYNTAX, FUNCTIONS AND STATEMENTS/excercise.js
+++ 01. SYNTAX, FUNCTIONS AND STATEMENTS/excercise.js
@@ -77,36 +77,36 @@ function roadRadar(input) {
         case 'motorway':
             if(speed>130 && speed<=150){
                 console.log('speeding');
-            } else if(speed>150 && speed<=190){
+            } else if(speed>150 && speed<=170){
                 console.log('excessive speeding');
-            } else if(speed>190){
+            } else if(speed>170){
                 console.log('reckless driving');
             }
             break;
         case 'interstate':
             if(speed>90 && speed<=110){
                 console.log('speeding');
-            } else if(speed>110 && speed<=150){
+            } else if(speed>110 && speed<=130){
                 console.log('excessive speeding');
-            } else if(speed>150){
+            } else if(speed>130){
                 console.log('reckless driving');
             }
             break;
         case 'city':
             if(speed>50 && speed<=70){
                 console.log('speeding');
-            } else if(speed>70 && speed<=110){
+            } else if(speed>70 && speed<=90){
                 console.log('excessive speeding');
-            } else if(speed>110){
+            } else if(speed>90){
                 console.log('reckless driving');
             }
             break;
         case 'residential':
             if(speed>20 && speed<=40){
                 console.log('speeding');
-            } else if(speed>40 && speed<=80){
+            } else if(speed>40 && speed<=60){
                 console.log('excessive speeding');
-            } else {
+            } else if(speed>60){
                 console.log('reckless driving');
             }
             break;
