df5bdb84de8e468e96b8913745a79ccf8ee1c61d
StelianDimitrov <Brglz@users.noreply.github.com>
2020-05-21 21:09:25 +0300

    Create lab.js
    Adding Sum First and Last Excercise

---
+++ 02. Arrays/lab.js
@@ -0,0 +1,10 @@
+//EXCERCISE 1
+
+/*function sumFirstAndLast(input) {
+    let first = +input[0];
+    let last = +input[input.length-1];
+    console.log(first+last);  
+}
+
+sumFirstAndLast(['40'])*/
+//-------------------------------------------------------------------------------------------------------------------------
