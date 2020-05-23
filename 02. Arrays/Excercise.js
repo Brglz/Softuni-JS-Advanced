1ded7d6a60b81b3c12578aacfdae95ab22cde3bb
StelianDimitrov <Brglz@users.noreply.github.com>
2020-05-23 16:55:28 +0300

    Create Excercise.js
    Adding Array With Given Delimiter Excercise and N-th Element From Array Excercise

---
+++ 02. Arrays/Excercise.js
@@ -0,0 +1,31 @@
+//EXCERCISE 1
+
+/*function arrayWithGivenDelimiter(input) {
+    let delimiter = input.pop();
+    console.log(input.join(delimiter));  
+}
+
+arrayWithGivenDelimiter(['One', 
+'Two', 
+'Three', 
+'Four', 
+'Five', 
+'-'])*/
+//-------------------------------------------------------------------------------------------------------------------------
+
+//EXCERCISE 2
+
+/*function nThElementFromArray(input) {
+    let step = +input.pop();
+    for (let i = 0; i < input.length; i+=step) {
+        console.log(input[i]);
+    }
+}
+
+nThElementFromArray(['1', 
+'2',
+'3', 
+'4', 
+'5', 
+'6'])*/
+//-------------------------------------------------------------------------------------------------------------------------
