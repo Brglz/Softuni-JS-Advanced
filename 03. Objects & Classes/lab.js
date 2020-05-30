90acd1aed308db03223525b598136527d830d9c0
StelianDimitrov <Brglz@users.noreply.github.com>
2020-05-30 15:26:40 +0300

    Update lab.js

--- 03. Objects & Classes/lab.js
+++ 03. Objects & Classes/lab.js
@@ -40,4 +40,57 @@ townsToJSON(['| Town York | Latitude | Longitude |',
 townsToJSON(['| Town | Latitude | Longitude |',
   '| Delhi |  28.38 | 77.12 |',
   '| Is it a real town? | 71.42 | -13.91 |'])*/
-  //-------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------
+
+//EXCERCISE 2
+
+/*function sumByTown(params) {
+  let obj = {};
+  for (let i = 0; i < params.length; i++) {
+    if (i % 2 == 0) {
+      if(obj[params[i]]===undefined){
+        obj[params[i]] = +params[i+1];
+      } else {
+        obj[params[i]] += +params[i+1]
+      }
+    }
+
+  }
+  console.log(JSON.stringify(obj));
+  
+};
+
+sumByTown(['Sofia','20','Varna','3','Sofia','5','Varna','4'])*/
+//-------------------------------------------------------------------------------------------------------------------------
+
+//EXCERCISE 3
+
+function populationInTowns(input) {
+  let obj = {};
+  for (const line of input) {
+    let [town,pop] = line.split(' <-> ');
+    if(obj[town]===undefined){
+      obj[town] = +pop;
+    } else {
+      obj[town] += +pop;
+    }
+    
+  }
+  
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      console.log(`${key} : ${obj[key]}`);
+      
+      
+    }
+  }
+  
+}
+
+populationInTowns(['Sofia <-> 1200000',
+'Montana <-> 20000',
+'New York <-> 10000000',
+'Washington <-> 2345000',
+'Las Vegas <-> 1000000',
+'Las Vegas <-> 1000099']
+)
