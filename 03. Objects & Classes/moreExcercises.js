9015d698457d30a297fe3a19344eff0625f243d5
StelianDimitrov <Brglz@users.noreply.github.com>
2020-06-01 20:24:40 +0300

    Create moreExcercises.js

---
+++ 03. Objects & Classes/moreExcercises.js
@@ -0,0 +1,28 @@
+//EXCERCISE 1
+
+/*class Rectangle {
+    constructor(width,height,color){
+        this.width = +width;
+        this.height = +height;
+        this.color = color;
+        
+    }
+    calcArea() {
+        return this.width * this.height
+    }
+}
+
+let rect = new Rectangle(4, 5, 'red');
+console.log(rect.width);
+console.log(rect.height);
+console.log(rect.color);
+console.log(rect.calcArea());*/
+//-------------------------------------------------------------------------------------------------------------------------
+
+//EXCERCISE 2
+
+function scoreToHTML(input) {
+    
+}
+
+scoreToHTML(['[{"name":"Pesho","score":479},{"name":"Gosho","score":205}]'])
