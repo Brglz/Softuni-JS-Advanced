829ae5847e28cb89c6358cad7dc4567f41e8876a
StelianDimitrov <Brglz@users.noreply.github.com>
2020-06-09 12:51:14 +0300

    Create app.js

---
+++ 05. DOM Manipulations/Lab/07. Dynamic-Validation/app.js
@@ -0,0 +1,19 @@
+function validate() {
+    const emailInput = document.getElementById('email');
+    emailInput.addEventListener('change', function(){
+        emailInput.classList.remove('error')
+        const value = emailInput.value;
+        if(value.match(/^[a-z-\.]+@[a-z-\.]+\.[a-z]{2,4}/)) {return;};
+        emailInput.classList.add('error');
+    });
+};
+
+// function validate() {
+//   const emailInput = document.getElementById('email');
+//   emailInput.addEventListener('change', function(){
+//       emailInput.classList.remove('error');
+//       const value = emailInput.value;
+//       if(value.match(/^[a-z-\.]+@[a-z-\.]+\.[a-z]{2,4}/)) { return; };
+//       emailInput.classList.add('error');
+//   })  
+// };
