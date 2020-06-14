31076472973db1cbf97e6f5ee010b5e2d0587f7a
StelianDimitrov <Brglz@users.noreply.github.com>
2020-06-14 21:21:11 +0300

    Update index.js

--- 06. Function Context/Excercise/index.js
+++ 06. Function Context/Excercise/index.js
@@ -1,3 +1,21 @@
-function solve(){
-   // TODO
+function solve() {
+
+   const tbody = document.querySelector('tbody');
+   const items = tbody.querySelectorAll('tr')
+   tbody.addEventListener('click', parseTable);
+
+   function parseTable(e) {
+      const row = e.target.parentNode;
+
+      if (row.nodeName === 'TR') {
+
+         if (row.style.backgroundColor !== '') {
+            row.style.backgroundColor = '';
+         } else {
+            [...items].forEach(x => x.style.backgroundColor = '')
+            row.style.backgroundColor = '#413f5e';
+         }
+
+      }
+   }
 }
