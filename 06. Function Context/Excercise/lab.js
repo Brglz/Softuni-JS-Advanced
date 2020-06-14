63c0f7620db78721522f4ce946c72294db216f49
StelianDimitrov <Brglz@users.noreply.github.com>
2020-06-14 21:20:58 +0300

    Update lab.js

--- 06. Function Context/Excercise/lab.js
+++ 06. Function Context/Excercise/lab.js
@@ -94,6 +94,50 @@ console.log(c.bestDepartment());
 
 //EXCERCISE 3
 
+/*class Hex {
+    constructor(value){
+        this.value = value;
+
+    }
+
+   
+    valueOf() {
+        return this.value;
+    }
+
+    toString(){
+        return `0x${(this.value.toString(16).toUpperCase())}`
+    }
+
+    plus(number){
+        return new Hex(this.value+number);
+    }
+
+    minus(number){
+        return new Hex(this.value-number);
+    }
+
+    static parse(string){
+        
+    }
+}
+
+let FF = new Hex(255);
+console.log(FF.toString());
+console.log(FF.valueOf() + 1 == 256);
+let a = new Hex(10);
+let b = new Hex(5);
+console.log(a.plus(b).toString());
+console.log(a.plus(b).toString()==='0xF');*/
+//-------------------------------------------------------------------------------------------------------------------------
+
+//EXCERCISE 4
+
+// index.js
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//EXCERCISE 5
 
 
 
