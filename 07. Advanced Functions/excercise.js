d8202b9a19cc08d194ec43ade15914dc36381cb1
StelianDimitrov <Brglz@users.noreply.github.com>
2020-06-15 22:52:15 +0300

    Update excercise.js

--- 07. Advanced Functions/excercise.js
+++ 07. Advanced Functions/excercise.js
@@ -19,7 +19,7 @@ console.log(solve([14, 7, 17, 6, 8], 'desc'))*/
 
 //EXCERCISE 2
 
-function solve() {
+/*function solve() {
     const counter = {}
 
     for (const arg of arguments) {
@@ -34,4 +34,80 @@ function solve() {
     Object.entries(counter).sort((a,b) => b[1]-a[1]).forEach(a => console.log(a.join(' = ')));    
 }
 
-solve({ name: 'bob'}, 3.333, 9.999)
+solve({ name: 'bob'}, 3.333, 9.999)*/
+//-------------------------------------------------------------------------------------------------------------------------
+
+//EXCERCISE 3
+
+function personalBMI(params) {
+    
+}
+
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//EXCERCISE 4
+
+/*(function solve() {
+    function add(a,b) {
+        const x1 = a[0];
+        const y1 = a[1];
+        const x2 = b[0];
+        const y2 = b[1];
+
+        const x = x1 + x2;
+        const y  = y1 + y2;
+
+        return [x,y];
+    }
+
+    function multiply(a,s) {
+        const x1 = a[0];
+        const y1 = a[1];
+
+        return [(x1*s),(y1*s)];
+    }
+
+    function length(a) {
+        const x1 = a[0];
+        const y1 = a[1];
+        const result = Math.sqrt((x1*x1)+(y1*y1));
+        return result
+    }
+
+    function dot(a,b) {
+        const x1 = a[0]; //2
+        const y1 = a[1]; //3
+        const x2 = b[0]; //2
+        const y2 = b[1]; //-1
+
+        return x1 * y1 + x2 * y2;
+    }
+
+    function cross(a,b) {
+        const x1 = a[0];
+        const y1 = a[1];
+        const x2 = b[0];
+        const y2 = b[1];
+
+        return (x1*y2) - (x2*y1);
+    }
+
+    const solution = {
+        add,
+        multiply,
+        length,
+        dot,
+        cross,
+    }
+    return solution
+})()
+
+
+
+console.log(solve.add([1, 1], [1, 0]))
+console.log(solve.multiply([3.5, -2], 2))
+console.log(solve.length([3, -4]))
+console.log(solve.dot([1, 0], [0, -1]))
+console.log(solve.cross([3, 7], [1, 0]))*/
+//-------------------------------------------------------------------------------------------------------------------------
