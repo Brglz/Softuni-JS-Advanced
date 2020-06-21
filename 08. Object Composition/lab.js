2b497df7adf1c6b16bf99d74131f4b4c93f22a03
StelianDimitrov <Brglz@users.noreply.github.com>
2020-06-21 21:23:46 +0300

    Update lab.js

--- 08. Object Composition/lab.js
+++ 08. Object Composition/lab.js
@@ -53,3 +53,73 @@ solve(['add JSFundamentals', 'print', 'add JSAdvanced', 'print','add JSApplicati
 //-------------------------------------------------------------------------------------------------------------------------
 
 //EXCERCISE 3
+
+/*function solve(input) {
+    const asd = JSON.parse(input);
+    
+    return asd.reduce(function(acc,curr) {
+        const {canMove} = curr;
+        console.log(canMove);
+        
+
+        console.log(acc);
+        console.log(curr);
+        
+        
+        
+    }, {})
+}
+
+solve(`[{"canMove": true},{"canMove":true, "doors": 4},{"capacity": 5}]`)*/
+//-------------------------------------------------------------------------------------------------------------------------
+
+//EXCERCISE 4
+
+function cars(commands) {
+    const commandMap = {
+        create: (acc, name, _, inheritName) => {
+            if (!inheritName) {
+                acc[name] = {};
+                return acc;
+            }
+            const parrent = acc[inheritName];
+            acc[name] = Object.create(parrent)
+            return acc
+        },
+        set: (acc, name, propName, propValue) => {
+            acc[name][propName] = propValue;
+            return acc;
+        },
+        print: (acc,name) => {
+            let result = '';
+
+            Object.keys(acc[name]).forEach(item => console.log(item));
+            
+            // Object.keys(acc).forEach((item, index, items) => {
+            //     for (let a in acc[item]) {
+            //         result += `${a}:${acc[item][a]}`
+            //     }
+            //     if (index !== items.length - 1) {
+            //         result += ', '
+
+            //     }
+            // })
+
+            console.log(result);
+            return acc
+        },
+    }
+
+    return commands.reduce((acc, currCommand) => {
+        const [op, v1, v2, v3] = currCommand.split(' ')
+        return commandMap[op](acc, v1, v2, v3)
+    }, {})
+}
+
+cars(['create c1',
+    'create c2 inherit c1',
+    'set c1 color red',
+    'set c2 model new',
+    'print c1',
+    'print c2']
+)
